c78ccbab8f318e32e45d25aee4cb9a3a0054dd5c
Vinicius98234 <pollyanatrader@gmail.com>
2023-11-11 15:33:35 -0300

    Q14 FINALIZADA

--- lista_exercicios_01/q14.py
+++ lista_exercicios_01/q14.py
@@ -1,14 +1,14 @@
-# item a
+# item a imprime A mensagem “Hello world!
 print("Hello world!")
 
-# item b
+# item b Imprime as mensagens "Hello" e "world" separadas por um espaço, devido à vírgula.
 print("hello", "world")
 
-# item c
+# item c Imprime a mensagem "Hello!", pula para a próxima linha (representado por \n), e continua com "My name is John" na linha seguinte.
 print("Hello!""\n My name is John")
 
-# item d
+# item d Imprime a mensagem "Hello!", usando uma tabulação (representada por \t), e continua com "My name is John" na mesma linha.
 print("Hello!""\t My name is Jonh")
 
-# item e
-print("Um dia, ela disse: "Estarei aqui por voce/".")
+# item e Imprime a mensagem que inclui aspas duplas internas usando a barra invertida (\") para escapar as aspas dentro da string. Isso evita que o Python interprete erroneamente o final da string.
+print("Um dia, ela disse: \"Estarei aqui por voce.\"")
