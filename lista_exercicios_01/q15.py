2f61cab272ec31043171b0b694bffd5a0efd9544
Vinicius98234 <pollyanatrader@gmail.com>
2023-11-11 15:47:15 -0300

    Q15 FINALIZADA

--- lista_exercicios_01/q15.py
+++ lista_exercicios_01/q15.py
@@ -1,1 +1,7 @@
-print ("olá mundo")
+# exemplo usando aspas triplas
+informações= """Nome: Vinicius
+Idade: 16
+Profissão: Desenvolvedor
+Linguagem Favorita: Python"""
+print (informações)
+# contém detalhes sobre uma pessoa em formato de texto estruturado. O uso de aspas triplas permite criar uma string multilinha de forma mais clara e organizada.
