e7c5da7c85808778df582db2bb8010b7317d3d96
Vinicius98234 <pollyanatrader@gmail.com>
2023-11-11 16:00:34 -0300

    Q19 FINALIZADA

---
+++ lista_exercicios_01/q19.py
@@ -0,0 +1,7 @@
+print("Menu de Opções:")
+print("1. Verificar saldo")
+print("2. Realizar saque")
+print("3. Realizar depósito")
+print("4. Sair")
+escolha = input("Escolha uma opção (1-4): ")
+print("Você escolheu a opção:", escolha)
